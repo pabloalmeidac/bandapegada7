07d02f5d28223ad5013340a6a2c810f175e37dc9
Pablo Almeida <pabloalmeidac@hotmail.com>
2022-06-28 02:53:57 -0300

    feat: add theme provider in app

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,15 @@
+import { Button } from "@mui/material";
 import { Navigate, Route, Routes } from "react-router-dom";
+import { useAppThemeContext } from "../shared/contexts/ThemeContext";
 
 export const AppRoutes = () => {
+  const { toggleTheme } = useAppThemeContext();
   return (
     <Routes>
-      <Route path="/home" element={<p> Pagina inicial</p>} />
+      <Route path="/home" element={<Button variant="contained" color="primary" onClick={ toggleTheme }>Mudar tema</Button>} />
 
       <Route path="*" element={ <Navigate to="/home" />} />
+
     </Routes>
   );
 }
