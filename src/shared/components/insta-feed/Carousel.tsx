a7a0afd767cf7669ece23354d79dbe813e79041b
Pablo Almeida <pabloalmeidac@hotmail.com>
2022-08-16 21:43:39 -0300

    feat: create carousel for feed instagram

---
+++ src/shared/components/insta-feed/Carousel.tsx
@@ -0,0 +1,60 @@
+import React, { useState, Fragment } from 'react';
+import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
+import './style.css';
+
+interface IFeedItem {
+  id: string;
+  media_type: 'IMAGE' | 'VIDEO';
+  media_url: string;
+  permalink: string;
+}
+
+interface IMyProps {
+  slides: IFeedItem[],
+}
+
+export const Carousel: React.FC<IMyProps> = ( props : IMyProps) => {
+  const [current, setCurrent] = useState(0);
+  const length = props.slides.length;
+
+  const nextSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1);
+  };
+
+  const prevSlide = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1);
+  };
+
+  if (!Array.isArray(props.slides) || props.slides.length <= 0) {
+    return null;
+  }
+
+  return (
+    <div className='container'>
+      <div className='container-left'>
+        <IoIosArrowBack className="left-arrow" onClick={prevSlide}/>
+      </div>
+      <div className='container-feed'>
+        {props.slides.map((item, index) => (
+          item.media_type === 'IMAGE' && index === current ? (
+            <Fragment key={index}>
+              <img src={ item.media_url } alt="foto instagram"/>
+              <a href={item.permalink} target="_blank" rel="noreferrer"> Ver postagem </a>
+            </Fragment>
+          ): item.media_type === 'VIDEO' && index === current ?(
+            <Fragment key={index}>
+              <iframe 
+                title="instagram-video"
+                src={ item.media_url }
+              />
+              <a href={item.permalink} target="_blank" rel="noreferrer"> Ver postagem </a>
+            </Fragment>
+          ) : null
+        ))}
+      </div>
+      <div className='container-right'>
+        <IoIosArrowForward  className="right-arrow" onClick={nextSlide}/>
+      </div>
+    </div>
+  );
+};
---
+++ src/shared/components/insta-feed/InstaFeed.tsx
@@ -0,0 +1,34 @@
+import { Box, Typography } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { getInstaFeed } from '../../services/api';
+import { Carousel } from './Carousel';
+
+interface IFeedItem {
+  id: string;
+  media_type: 'IMAGE' | 'VIDEO';
+  media_url: string;
+  permalink: string;
+}
+
+export const InstaFeed: React.FC = () => {
+  const [feedList, setFeedList] = useState<IFeedItem[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await getInstaFeed();
+      setFeedList(response);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <Box sx={{ display: 'flex-block', width: '100vw', height:'70vh' }}>
+      <Typography variant="h1" fontSize={24} fontWeight="bold" lineHeight={1.8} letterSpacing={2} textAlign="center" >
+          Estamos no instagram
+      </Typography>
+      <Box sx={{ display: 'flex', width: '100%', flexWrap: 'wrap', height:'50%', flexDirection: 'row', gap: '1rem' }}>
+        {feedList && <Carousel slides={feedList} />}
+      </Box>
+    </Box>
+  );
+};
